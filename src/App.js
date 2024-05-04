7a98d2e346f8be213f37f29055e8111ee994f7a5
Bizz-Git <lucabizzotto2003@gmail.com>
2024-05-04 15:59:01 +0200

    chat funzionante

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
 
-function App() {
+// Connection to the server
+const socket = io.connect('http://localhost:4000');
+
+const App = () => {
+  const [name, setName] = useState('');
+  const [message, setMessage] = useState('');
+  const [messageReceived, setMessageReceived] = useState({});
+  const [messageHistory, setMessageHistory] = useState([]);
+
+  const [room, setRoom] = useState('');
+
+  const joinRoom = () => {
+    if (room !== '') {
+      socket.emit('join_room', room)
+    }
+  }
+
+  const sendMessage = () => {
+    socket.emit('send_message', { username: name, message, room });
+  }
+
+  useEffect(() => {
+    // Listen the events -- Ascolta l'evento per ricevere i messaggi solo una volta
+    const handleMessage = (e_data) => {
+      setMessageReceived({ from: e_data.username, message: e_data.message });
+      // Add new message to history
+      setMessageHistory(prevMessages => [...prevMessages, e_data.message]);
+    };
+
+    socket.on('receive_message', handleMessage);
+
+    // Clean up 
+    return () => {
+      socket.off('receive_message', handleMessage);
+    };
+  }, [socket]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='app'>
+      <input placeholder='Nome' onChange={ (event) => { setName(event.target.value) } } />
+      <br />
+      <input placeholder='Stanza' onChange={ (event) => { setRoom(event.target.value) } } />
+      <button onClick={ joinRoom }> Unisciti </button>
+      <br />
+      <input placeholder='Messaggio' onChange={ (event) => { setMessage(event.target.value) } } />
+      <button onClick={ sendMessage }> Invia Messaggio </button>
+      <h3>Ultimo messaggio da
+        { messageReceived.from && ` ${messageReceived.from}: ${messageReceived.message}` }
+      </h3>
+
+      <ul>
+        {messageHistory.map((message, index) => (
+          <li key={index}>{message}</li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
